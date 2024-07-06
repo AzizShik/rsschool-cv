0dfd8599aa948c5558cfcebc9ee9c3d9cb2ec683
Abdyl-Aziz <azizshihabov@mail.ru>
2024-07-06 20:33:02 +0300

    feat: finish html cv

--- js/script.js
+++ js/script.js
@@ -17,20 +17,36 @@ const burgerTiming = {
 	iterations: 1,
 };
 
+function closeBurger() {
+	headerNav.classList.remove('header__nav--active');
+	headerBurger.classList.remove('header__burger--active');
+	body.classList.remove('lock');
+}
+
+function OpenBurger() {
+	headerNav.classList.add('header__nav--active');
+	headerBurger.classList.add('header__burger--active');
+	body.classList.add('lock');
+	headerNav.animate(burgerOpenTransform, burgerTiming);
+}
+
 headerBurger.addEventListener('click', (e) => {
 	const el = e.target;
 	if (el.parentNode.classList.contains('header__burger--active')) {
 		let anim = headerNav.animate(burgerCloseTransform, burgerTiming);
 
 		anim.addEventListener('finish', () => {
-			headerNav.classList.remove('header__nav--active');
-			headerBurger.classList.remove('header__burger--active');
-			body.classList.remove('lock');
+			closeBurger();
 		});
 	} else {
-		headerNav.classList.add('header__nav--active');
-		headerBurger.classList.add('header__burger--active');
-		body.classList.add('lock');
-		headerNav.animate(burgerOpenTransform, burgerTiming);
+		OpenBurger();
+	}
+});
+
+headerNav.addEventListener('click', (e) => {
+	const el = e.target;
+	console.log(el);
+	if (el.classList.contains('header__nav-list-item-link')) {
+		closeBurger();
 	}
 });
